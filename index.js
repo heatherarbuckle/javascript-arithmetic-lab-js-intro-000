3f18964be3ebe8474ff1d62dc8f8c4fc0591ed6c
Heather Arbuckle <bucklehead@gmail.com>
2019-05-14 15:04:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ function divide(a, b) {
 }
 
 function inc(n) {
-  n += 1;
+  return n += 1;
 }
 
 function dec(n) {
-  n -= 1;
+  return  n -= 1;
 }
 
 function makeInt(n) {
-  return parseInt(n);
+  return parseInt(n, 10);
 }
 
 function preserveDecimal(n) {
